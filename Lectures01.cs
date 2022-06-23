7e73193543938e6ccb3d10ba9b7987ff66d0e3c7
L.E.A Samadhi Dewmini <73785369+samadhidew@users.noreply.github.com>
2022-06-23 20:54:06 +0530

    Create Lectures01.cs

---
+++ Lectures01.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Lectures01 : Form
+    {
+        public Lectures01()
+        {
+            InitializeComponent();
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bunifuThinButton24_Click(object sender, EventArgs e)
+        {
+            ChooseForm cf = new ChooseForm();
+            cf.Show();
+            this.Hide();
+        }
+
+        private void bunifuThinButton22_Click(object sender, EventArgs e)
+        {
+            LectureReg LecR = new LectureReg();
+            LecR.Show();
+            this.Hide();
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            LecViewDetal LecV = new LecViewDetal();
+            LecV.Show();
+            this.Hide();
+        }
+
+        private void bunifuThinButton23_Click(object sender, EventArgs e)
+        {
+            LecRemv LecR = new LecRemv();
+            LecR.Show();
+            this.Hide();
+        }
+    }
+}
